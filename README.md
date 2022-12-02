d666321a14c348b2f86d797d8d36461861e9073a
Hyunseok Byun <gumyoincirno@gmail.com>
2022-12-02 21:43:17 +0900

    Backend template initializing

---
+++ README.md
@@ -0,0 +1,4 @@
+### 백엔드
+- compose파일 확인 중
+- Entity/DTO 일요일까지 완성 예정
+- application.properties 파일은 디스코드에서 말씀주세용
---
+++ src/main/java/dev/aco/back/BackApplication.java
@@ -0,0 +1,13 @@
+package dev.aco.back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackApplication.class, args);
+	}
+
+}
