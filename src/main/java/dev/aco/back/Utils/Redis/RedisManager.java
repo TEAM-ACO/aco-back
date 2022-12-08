2ed32ff0a45eaaba8a8e3df00e78e8b6d46e1c3a
김형준 <porkbellyweb@gmail.com>
2022-12-09 00:58:45 +0900

    '[백앤드]이메일인증'

--- src/main/java/dev/aco/back/Utils/Redis/RedisManager.java
+++ src/main/java/dev/aco/back/Utils/Redis/RedisManager.java
@@ -38,10 +38,13 @@ public class RedisManager {
 
     @Transactional
     public Boolean TokenValidator(String accessToken, String refreshToken) {
-        Optional<Member> targetMember = mrepo.findById(Long.parseLong(manager.tokenParser(accessToken).get("userNumber").toString()));
+        Optional<Member> targetMember = mrepo
+                .findById(Long.parseLong(manager.tokenParser(accessToken).get("userNumber").toString()));
         Long accessTokenMemberId = targetMember.orElse(Member.builder().memberId(0L).build()).getMemberId();
-        Long refreshTokenMemberId = Long.parseLong(Optional.ofNullable(redisTemplate.opsForValue().get(refreshToken)).orElse("-1"));
-        if (refreshTokenMemberId == accessTokenMemberId & manager.tokenValidator(refreshToken) & isLogged(accessToken)) {
+        Long refreshTokenMemberId = Long
+                .parseLong(Optional.ofNullable(redisTemplate.opsForValue().get(refreshToken)).orElse("-1"));
+        if (refreshTokenMemberId == accessTokenMemberId & manager.tokenValidator(refreshToken)
+                & isLogged(accessToken)) {
             return true;
         } else {
             removeRefreshToken(refreshToken);
@@ -57,5 +60,35 @@ public class RedisManager {
     public Boolean isLogged(String aToken) {
         return mrepo.findById(Long.parseLong(manager.tokenParser(aToken).get("userNumber").toString()))
                 .orElse(Member.builder().memberId(0L).logged(false).build()).getLogged();
+    }
+
+    // Q1. 이 클래스에 아래 코드를 작성하는게 맞나요? 아니면 따로 클래스를 만들어야 할까요?
+
+    // 이메일인증 (저장, 유효기간 설정, 삭제)
+    // Redis에 들어가는 DATA (Key : authnum / Value : email)
+    public String getEmailData(String key) { // authnum 을 통해 email을 얻는다.
+        ValueOperations<String, String> valueOperations = redisTemplate.opsForValue();
+        return valueOperations.get(key);
+    }
+
+    public void setEmailData(String key, String value) { // authnum 을 통해 email을 얻는다.
+        ValueOperations<String, String> valueOperations = redisTemplate.opsForValue();
+        valueOperations.set(key, value);
+    }
+
+    // Redis에 저장하려고하다보니 유효기간 저장하는부분에서 문제가 생겼습니다.
+    // 검색해보면서 이런식으로 저장을 하는걸 보고 제가 이해하는 선에서 적어봤습니다.
+    // 이것이 문제가 될지 잘 모르겠습니다. 만약 이것을 사용한다면 기존 MailService에서 작성한
+    // 유효기간 관련된 코드는 사용할 필요가 없을것 같음.
+    public void setDataExpire(String key, String value, long expiration) {
+        ValueOperations<String, String> valueOperations = redisTemplate.opsForValue();
+        // expireDuaration 동안 (key, value)를 저장한다.
+        Duration expireDuration = Duration.ofMillis(expiration);
+        valueOperations.set(key, value, expireDuration);
+    }
+
+    public void deleteEmailData(String key) {
+        // 데이터 삭제
+        redisTemplate.delete(key);
     }
 }
--- src/main/java/dev/aco/back/service/MailService/MailServiceImpl.java
+++ src/main/java/dev/aco/back/service/MailService/MailServiceImpl.java
@@ -8,52 +8,68 @@
 
 // import dev.aco.back.Entity.User.emailAuth;
 // import dev.aco.back.Repository.MailRepository;
+// import dev.aco.back.Utils.Redis.RedisManager;
 // import jakarta.transaction.Transactional;
 // import lombok.RequiredArgsConstructor;
 
 // @Service
 // @RequiredArgsConstructor
 // public class MailServiceImpl implements MailService {
-//   private final AcoMailSender mailSender;
-//   private final MailRepository mrepo;
+// private final AcoMailSender mailSender;
+// private final MailRepository mrepo;
+// private final RedisManager redisManager;
 
-//   @Override
-//   public boolean mailCheckingRequest(String email) {
+// @Override
+// public boolean mailCheckingRequest(String email) {
 
-//     // 인증번호 생성
-//     Integer authnum = (int) (Math.floor(Math.random() * 10000) - 1);
-//     // 인증 객체
-//     Optional<emailAuth> isCheck = mrepo.getByEmail(email);
-//     // 존재한다면
-//     if (isCheck.isPresent()) {
-//       mrepo.delete(isCheck.get());
-//     }
-//     mrepo.save(emailAuth.builder().email(email).authNum(authnum).isAuthrized(false).build());
-//     mailSender.send(email, "[ACO] 이메일 인증을 위한 인증번호를 안내드립니다.", "인증번호 : " + authnum);
-//     return true;
-//   }
+// // 인증번호 생성
+// Integer authnum = (int) (Math.floor(Math.random() * 10000) - 1);
+// // 인증 객체
+// Optional<emailAuth> isCheck = mrepo.getByEmail(email);
+// // 존재한다면
+// if (isCheck.isPresent()) {
+// mrepo.delete(isCheck.get());
+// }
+// mrepo.save(emailAuth.builder().email(email).authNum(authnum).isAuthrized(false).build());
+// mailSender.send(email, "[ACO] 이메일 인증을 위한 인증번호를 안내드립니다.", "인증번호 : " +
+// authnum);
+// //여기서 redisManager.setDataExpire()매서드 실행하면서 유효기간 저장함.
+// return true;
+// }
 
-//   @Override
-//   @Transactional
-//   public String mailCheckingNumCheckRequest(String email, Integer authNum) {
-//     Optional<emailAuth> isCheck = mrepo.getByEmail(email);
-//     if (isCheck.isPresent()) {
-//       LocalDateTime now = LocalDateTime.now();
-//       LocalDateTime pastTime = isCheck.get().getCreatedDateTime().plusMinutes(3);
-//       if (ChronoUnit.SECONDS.between(now, pastTime) <= 0) {
-//         return "인증시간이 만료되었습니다";
-//       }
-//       if (isCheck.get().getAuthNum().intValue() == authNum.intValue() && isCheck.get().getIsAuthrized() == false) {
-//         mrepo.updateAuthTime(isCheck.get().getEauthId(), isCheck.get().getCreatedDateTime().plusMinutes(60));
-//         return "인증이 완료되었습니다 1시간 이내에 가입을 완료해주세요";
-//       } else if (isCheck.get().getAuthNum().intValue() == authNum.intValue()
-//           && isCheck.get().getIsAuthrized() == true) {
-//         return "이미 인증된 이메일입니다 가입을 완료해주세요";
-//       } else {
-//         return "인증 번호가 잘못되었습니다";
-//       }
-//     } else {
-//       return "잘못된 접근입니다";
-//     }
-//   }
+// @Override
+// @Transactional
+// public String mailCheckingNumCheckRequest(String email, Integer authNum) {
+// Optional<emailAuth> isCheck = mrepo.getByEmail(email);
+// if (isCheck.isPresent()) {
+// LocalDateTime now = LocalDateTime.now();
+// LocalDateTime pastTime = isCheck.get().getCreatedDateTime().plusMinutes(3);
+// if (ChronoUnit.SECONDS.between(now, pastTime) <= 0) {
+// return "인증시간이 만료되었습니다";
+// }
+// if (isCheck.get().getAuthNum().intValue() == authNum.intValue() &&
+// isCheck.get().getIsAuthrized() == false) {
+// mrepo.updateAuthTime(isCheck.get().getEauthId(),
+// isCheck.get().getCreatedDateTime().plusMinutes(60));
+// return "인증이 완료되었습니다 1시간 이내에 가입을 완료해주세요";
+// } else if (isCheck.get().getAuthNum().intValue() == authNum.intValue()
+// && isCheck.get().getIsAuthrized() == true) {
+// return "이미 인증된 이메일입니다 가입을 완료해주세요";
+// } else {
+// return "인증 번호가 잘못되었습니다";
+// }
+// } else {
+// return "잘못된 접근입니다";
+// }
+// }
+
+// //레퍼런스 참고하면서 임시로 만든 매서드 입니다. 이런식으로 되야하는지 잘 모르겠습니다.
+// public boolean verifyEmail(String key){
+// String email = redisManager.getEmailData(key);
+// if(email == null){
+// redisManager.deleteEmailData(key);
+// return false;
+// }
+// return true;
+// }
 // }
