f7c5a13947e8f5b1c95a35347e87fe1db1bc8930
Hyunseok Byun <gumyoincirno@gmail.com>
2022-12-12 00:01:28 +0900

    Implement swagger for apidoc

--- src/main/java/dev/aco/back/Repository/MemberRepository.java
+++ src/main/java/dev/aco/back/Repository/MemberRepository.java
@@ -2,14 +2,12 @@ package dev.aco.back.Repository;
 
 import java.util.Optional;
 
-import org.springframework.stereotype.Repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
 import dev.aco.back.Entity.User.Member;
 
-@Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
     Boolean existsByEmail(String email);
--- src/main/java/dev/aco/back/Repository/ReplyRepository.java
+++ src/main/java/dev/aco/back/Repository/ReplyRepository.java
@@ -1,11 +1,9 @@
 package dev.aco.back.Repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import dev.aco.back.Entity.Article.Reply;
 
-@Repository
 public interface ReplyRepository extends JpaRepository<Reply, Long>{
     
 }
