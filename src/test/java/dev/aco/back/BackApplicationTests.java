78e3b52ddf1e22a5aac8b850901c11d700e4c84c
Hyunseok Byun <gumyoincirno@gmail.com>
2023-01-03 17:41:47 +0900

    Fix delete function for memeber

--- src/test/java/dev/aco/back/BackApplicationTests.java
+++ src/test/java/dev/aco/back/BackApplicationTests.java
@@ -30,6 +30,7 @@ import dev.aco.back.Entity.Enum.Menu;
 import dev.aco.back.Entity.Enum.Roles;
 import dev.aco.back.Entity.Linker.ArticleHashtag;
 import dev.aco.back.Entity.Report.ArticleReport;
+import dev.aco.back.Entity.Report.MemberReport;
 import dev.aco.back.Entity.User.Member;
 import dev.aco.back.Entity.Visitor.Visitor;
 import dev.aco.back.Repository.ArticleImageRepository;
@@ -38,6 +39,7 @@ import dev.aco.back.Repository.ArticleReportRepository;
 import dev.aco.back.Repository.ArticleRepository;
 import dev.aco.back.Repository.HashtagRepository;
 import dev.aco.back.Repository.LikeRepository;
+import dev.aco.back.Repository.MemberReportRepository;
 import dev.aco.back.Repository.MemberRepository;
 import dev.aco.back.Repository.ReplyRepository;
 import dev.aco.back.Repository.VisitorRepository;
@@ -66,6 +68,8 @@ class BackApplicationTests {
 	private ReplyRepository rrepo;
 	@Autowired
 	private ArticleReportRepository arrepo;
+	@Autowired
+	private MemberReportRepository mrrepo;
 	@Autowired
 	private ArticleImageRepository airepo;
 	@Autowired
@@ -99,6 +103,17 @@ class BackApplicationTests {
 				.userimg("basic.png")
 				.build());
 
+				Member membertwo = mrepo.save(Member.builder()
+				.email("2test@test.test")
+				.password(pEncoder.encode("1234"))
+				.nickname("2test")
+				.mobile("000000000")
+				.roleSet(roles)
+				.oauth(null)
+				.logged(true)
+				.userimg("basic.png")
+				.build());
+
 		Article article = arepo.save(Article
 				.builder()
 				.articleContext("test, test, test, test, test, test".getBytes())
@@ -126,7 +141,7 @@ class BackApplicationTests {
 		arrepo.save(ArticleReport.builder().article(article).articleReportContext("test")
 				.articleReportTitle("testtitle").articlereporter(member).build());
 		lrepo.save(ArticleLike.builder().liked(true).liker(member).article(article).build());
-
+		mrrepo.save(MemberReport.builder().memberreporter(member).reported(membertwo).userReportContext("asdf").userReportTitle("asdf").build());
 	}
 
 	// @Test
