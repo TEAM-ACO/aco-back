5ba5ca85baed5e7620c5eb4977288fa2ec4bfaca
Hyunseok Byun <gumyoincirno@gmail.com>
2023-02-03 17:45:17 +0900

    Add cors filter for ssr

--- src/main/java/dev/aco/back/Controller/ArticleController.java
+++ src/main/java/dev/aco/back/Controller/ArticleController.java
@@ -9,7 +9,6 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -33,7 +32,6 @@ public class ArticleController {
     private final ArticleService aser;
     private final ReplyService rser;
 
-    @CrossOrigin(origins = {"http://43.200.152.148:3075/", "http://localhost:3075/"})
     @PostMapping(value = "list", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<List<ArticleDTO>> getArticleList(@RequestBody pageVO vo) {
         Pageable pageable = PageRequest.of(vo.getRequestedPageNumber(), vo.getRequestedPageSize(), Sort.by(Direction.DESC, "articleId"));
